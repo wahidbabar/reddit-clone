a86bf8e7bd0c0f7c2a805cc31238ac1d2ec39706
Abdul Wahid <babarsahab4u@gmail.com>
2022-11-14 21:25:52 +0500

    mod 1

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -95,27 +95,8 @@ function PostPage() {
       <div className="-my-5 rounded-b-md border-t-0 border-gray-300 bg-white py-5 px-10">
         <hr className="py-2" />
 
-        {post?.comments?.map((comment) => (
-          <Comment comment={comment} key={comment.id} />
-          //   <div
-          //     className="relative flex items-center space-x-2 space-y-5"
-          //     key={comment.id}
-          //   >
-          //     <hr className="absolute top-10 left-7 z-0 h-16 border" />
-          //     <div className="z-50">
-          //       <Avatar seed={comment.username} />
-          //     </div>
-
-          //     <div className="flex flex-col">
-          //       <p className="py-2 text-xs text-gray-400">
-          //         <span className="font-semibold text-gray-600">
-          //           {comment.username}
-          //         </span>{" "}
-          //         <ReactTimeago date={comment.created_at} />
-          //       </p>
-          //       <p>{comment.text}</p>
-          //     </div>
-          //   </div>
+        {post?.comments?.map((comment, i) => (
+          <Comment comment={comment} key={i} />
         ))}
       </div>
     </div>
